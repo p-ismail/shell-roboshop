2bfaf69ecb442ca586666cf0ca4356db25f5dabf
ismail-7337 <ismailp1140@gmail.com>
2026-08-06 17:37:02 +0530

    shell-roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -22,7 +22,7 @@ do
             --query 'Reservations[].Instances[].PublicIpAddress' \
             --output text
         )
-        RECORD_NAME="$DOMAIN_NAME" # daws88s.online
+        RECORD_NAME="$DOMAIN_NAME" # daws70s.online
     else
         IP=$(
             aws ec2 describe-instances \
@@ -30,7 +30,7 @@ do
             --query 'Reservations[].Instances[].PrivateIpAddress' \
             --output text
         )
-        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws88s.online
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws70s.online
     fi
 
     echo "IP Address: $IP"
